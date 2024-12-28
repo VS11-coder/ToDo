6c45fa357269a8d6bf5442a471192b5a14cf3789
VS11-coder <vasudevsarraf11@gmail.com>
2024-12-28 13:58:36 +0530

    adding content

--- Backend/index.js
+++ Backend/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 const { createToDo, updateToDo } = require('./types');
-app.post('/todo', (req, res) => {
+const { Todo } = require('./db');
+
+app.post('/todo', async (req, res) => {
     const createPayload = req.body;
     const parsedPayload = createToDo.safeParse(createPayload);
     if(!parsedPayload.success) {
@@ -11,11 +13,25 @@ app.post('/todo', (req, res) => {
         })
         return;
     }
+    await Todo.create({
+        title: createPayload.title,
+        description: createPayload.description,
+        completed: false
+    });
+    res.json({
+        msg : "Todo created successfully",
+    });
 });
-app.get('/todos', (req, res) => {
 
+app.get('/todos', async (req, res) => {
+    const todos = await todos.find();
+    res.json(todos);
+    // Todo.findAll().then(todos => {
+    //     res.json(todos);
+    // }
 });
-app.put('/completed', (req, res) => {
+
+app.put('/completed', async (req, res) => {
     const updatePayload = req.body;
     const parsedPayload = updateToDo.safeParse(updatePayload);
     if(!parsedPayload.success) {
@@ -24,7 +40,16 @@ app.put('/completed', (req, res) => {
         })
         return;
     }
+    await Todo.updateOne({
+        _id: req.body.id
+    }, {
+        completed: true
+    });
+    res.json({
+        msg : "Todo marked as completed",
+    });
 });
+
 app.listen(3000, () => {   
     console.log('Server is running on port 3000');
 });
